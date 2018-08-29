121ec4842775b775a39cff231c4ddc4491a79589
Jan Ulferts <julferts@mesosphere.com>
2018-08-29 16:12:51 +0200

    Revert "init"
    This reverts commit ae657df6dc49467964bee92b110a48f7f2a49bec.

--- variables.tf
+++ variables.tf
@@ -211,7 +211,7 @@ variable "dcos_superuser_password_hash" {
   description = "[Enterprise DC/OS] set the superuser password hash (recommended)"
 }
 
-variable "dcos_name_prefix" {
+variable "dcos_cluster_name" {
   default     = ""
   description = "sets the DC/OS cluster name"
 }
