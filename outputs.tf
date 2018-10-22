08b9842a59c94b0020cce2863abc0123633a4fef
Jan Ulferts <julferts@mesosphere.com>
2018-10-22 14:07:06 +0200

    cleaning up input output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "script" {
   value       = "${data.template_file.script.rendered}"
-  description = "the battle-tested provisioner contents of the output by DC/OS role to perform requried admin actions in behalf of the user as documented in http://mesosphere.com and http://dcos.io"
+  description = "The battle-tested provisioner contents of the output by DC/OS role to perform requried admin actions in behalf of the user as documented in http://mesosphere.com and http://dcos.io"
 }
