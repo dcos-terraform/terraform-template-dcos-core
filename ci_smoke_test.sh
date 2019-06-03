ec97f7cfbdf8707c058c94475090dd36b8c1ee21
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-06-03 14:37:20 +0200

    facility management

--- ci_smoke_test.sh
+++ ci_smoke_test.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-# This part checks that the local version of tf_dcos_core does not break terraform-dcos
-mkdir .tf_dcos_core
-mv * .tf_dcos_core
-git remote add terraform-dcos https://github.com/dcos/terraform-dcos.git
-git fetch terraform-dcos
-git reset --hard terraform-dcos/master
-find . -name '*.tf' -exec sed -i 's|github.com/dcos/tf_dcos_core|../.tf_dcos_core|g' {} \;
-chmod +x ci_smoke_test.sh
-./ci_smoke_test.sh
-
-# This part checks that tf_dcos_core works on its own
-mv terraform .tf_dcos_core/terraform
-cd .tf_dcos_core
-export TERRAFORM_PATH=${PWD}/terraform
-${TERRAFORM_PATH} init
+terraform init
 # Open DC/OS Check
 for dir in ./open/dcos-versions/*
 do
@@ -25,8 +11,8 @@ do
     do
         role=${dir2%*/}
         role=${role##*/}
-        ${TERRAFORM_PATH} validate -var "dcos_type=open" -var "role=${role}" -var "dcos_version=${version}"
-        ${TERRAFORM_PATH} plan -var "dcos_type=open" -var "role=${role}" -var "dcos_version=${version}"
+        terraform validate -var "dcos_type=open" -var "role=${role}" -var "dcos_version=${version}"
+        terraform plan -var "dcos_type=open" -var "role=${role}" -var "dcos_version=${version}"
     done
 done
 
@@ -39,7 +25,7 @@ do
     do
         role=${dir2%*/}
         role=${role##*/}
-        ${TERRAFORM_PATH} validate -var "dcos_type=ee" -var "role=${role}" -var "dcos_version=${version}"
-        ${TERRAFORM_PATH} plan -var "dcos_type=ee" -var "role=${role}" -var "dcos_version=${version}"
+        terraform validate -var "dcos_type=ee" -var "role=${role}" -var "dcos_version=${version}"
+        terraform plan -var "dcos_type=ee" -var "role=${role}" -var "dcos_version=${version}"
     done
 done
--- ee/output.tf
+++ ee/output.tf
@@ -1,7 +1,3 @@
 output "script" {
   value = "${data.template_file.script.rendered}"
 }
-
-output "path" {
-  value = "${lookup(var.dcos_download_path, var.dcos_version, "download path non existant")}"
-}
