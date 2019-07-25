99ed56eb6624e44832865f083338fcd156ce3ec1
Sebastian Brandt <793580+sebbrandt87@users.noreply.github.com>
2019-07-25 09:48:10 +0200

    stable release 1.13.3

--- download-variables.tf
+++ download-variables.tf
@@ -24,6 +24,7 @@ variable "dcos_ee_download_path" {
     "1.13.0"       = "https://downloads.mesosphere.com/dcos-enterprise/stable/1.13.0/commit/1ec160b03e26d6e951083f524945c1ed2af3f3a3/dcos_generate_config.ee.sh"
     "1.13.1"       = "https://downloads.mesosphere.com/dcos-enterprise/stable/1.13.1/commit/5b1576c498bcebe234b7c68ad21e6a08101972fc/dcos_generate_config.ee.sh"
     "1.13.2"       = "https://downloads.mesosphere.com/dcos-enterprise/stable/1.13.2/dcos_generate_config.ee.sh"
+    "1.13.3"       = "https://downloads.mesosphere.com/dcos-enterprise/stable/1.13.3/dcos_generate_config.ee.sh"
     "master"       = "https://downloads.mesosphere.com/dcos-enterprise/testing/master/dcos_generate_config.ee.sh"
   }
 
@@ -91,6 +92,7 @@ variable "dcos_open_download_path" {
     "1.13.0"       = "https://downloads.dcos.io/dcos/stable/1.13.0/commit/f5cb8649583bb43d0f7ad5b0b7a12c5333e9587f/dcos_generate_config.sh"
     "1.13.1"       = "https://downloads.dcos.io/dcos/stable/1.13.1/commit/f59e02457b5cd64ef14f94f79eb92db15b2d91f6/dcos_generate_config.sh"
     "1.13.2"       = "https://downloads.dcos.io/dcos/stable/1.13.2/dcos_generate_config.sh"
+    "1.13.3"       = "https://downloads.dcos.io/dcos/stable/1.13.3/dcos_generate_config.sh"
     "master"       = "https://downloads.dcos.io/dcos/testing/master/dcos_generate_config.sh"
   }
 
