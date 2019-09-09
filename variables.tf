e45ef007568b9a3b7411611e41949a108bb816bd
Jan Ulferts <julferts@mesosphere.com>
2019-09-09 13:30:17 +0000

    CI - [ci-skip] - updated files

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "bootstrap_private_ip" {
 
 variable "dcos_version" {
   default     = "1.13.3"
-  description = "Specifies which DC/OS version instruction to use. Options: 1.13.3, 1.12.4, 1.11.10, etc. See dcos_download_path or dcos_version tree for a full list."
+  description = "Specifies which DC/OS version instruction to use. Options: 1.13.4, 1.12.4, 1.11.11, etc. See dcos_download_path or dcos_version tree for a full list."
 }
 
 variable "dcos_versions_service_url" {
